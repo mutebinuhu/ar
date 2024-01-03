09178226898c6e70f34e39c22ca326155615bec9
swaggp <mutebinuhu1@gmail.com>
2024-01-03 14:05:02 +0400

    chore: changes to the ui

--- app/components/Ad.js
+++ app/components/Ad.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Ad = ({info,em, action}) => {
     return (
-        <div className='bg-gray-100 p-4 m-4 text-center shadow-2xl rounded '>
-            <h3 class="text-xl">{info}<strong>{em}</strong></h3>
-            <button className='bg-blue-500 m-2 text-white rounded px-4 py-2'>{action}</button>
+        <div className='bg-gray-100 p-4 text-center shadow-2xl rounded '>
+            <h3 class="text-2xl leading-loose"> {info}<strong> {em}</strong></h3>
+            <button className='bg-blue-500 m-4 text-white rounded px-4 py-2'>{action}</button>
             
         </div>
     );
--- app/components/BookingOne.js
+++ app/components/BookingOne.js
@@ -4,16 +4,16 @@ import Ad from './Ad';
 const BookingOne = () => {
     return (
         <section className='px-2 py-6'>
-                <h2 className='font-bold text-4xl text-gray-800 text-center'>
-                Enriching {`Life's`} Chapters through Tailored <br/> <span className='bg-blue-500 text-gray-100'> Homecare Services</span>
+                <h2 className='font-bold text-4xl text-gray-800 text-center leading-snug'>
+                Enriching {`Life's`} Chapters through Tailored <br/> <span className='bg-blue-500 text-gray-100 px-2'> Homecare Services</span>
                     </h2>
-                    <div className='py-4 font-bold text-gray-800 text-center'>
-                        <p>Lets start the journey together</p>
-                        <div className='my-4'>
+                    <div className='py-4 font-medium text-gray-800 text-center'>
+                        <p className='text-xl'>Lets start the journey together</p>
+                        <div className='my-6'>
                             <button className='bg-blue-500 py-2 px-3 rounded text-white '>Book Now</button>
                         </div>
-                        <div className='my-4'>
-                            <button className='border-2 py-2 px-3 rounded border-blue-500 '>Looking for Job ?</button>
+                        <div className='my-6'>
+                            <button className='border-2 py-2 px-3 rounded border-blue-500 '>Looking for a Job ?</button>
                         </div>
                     </div>
                     <Ad action={`Book Now`} info={`We can provide live-in care in as little as`} em={`24 hours`}/>
