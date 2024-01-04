54f86e9650ed11d0a0882a62e11dacf5de63bdcf
swaggp <mutebinuhu1@gmail.com>
2024-01-04 15:45:02 +0400

    chore: few changes

--- app/components/Ad.js
+++ app/components/Ad.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Ad = ({info,em, action}) => {
     return (
-        <div className='bg-gray-100 p-4 text-center shadow-2xl rounded '>
+        <div className='bg-gray-100 p-4 text-center shadow-md rounded '>
             <h3 class="text-xl leading-relaxed"> {info}<strong> {em}</strong></h3>
             <button className='bg-blue-500 m-4 text-white rounded px-4 py-2'>{action}</button>
         </div>
